74ba07285244714533f94663fdfe6ab6bf8f547e
He11oW-10 <46309857+He11oW-10@users.noreply.github.com>
2022-03-10 18:13:08 +0800

    Update __init__.py

--- httptrigger1/__init__.py
+++ httptrigger1/__init__.py
@@ -5,7 +5,7 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
+    
     name = req.params.get('name')
     if not name:
         try:
@@ -14,7 +14,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             name = req_body.get('name')
-
+    
+    logging.info(name.isascii())
+    logging.info(name)
+    
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
