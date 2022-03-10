cc648bea83548167b8f0a8eee1c387fd97818548
He11oW-10 <46309857+He11oW-10@users.noreply.github.com>
2022-03-10 18:22:07 +0800

    Update __init__.py

--- httptrigger1/__init__.py
+++ httptrigger1/__init__.py
@@ -15,8 +15,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         else:
             name = req_body.get('name')
     
-    logging.info(name.isascii())
     logging.info(name)
+    logging.info(name.isascii())
+    logging.info(type(contenido))
+
     
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
